591512de1b5aefb8fa88e67a262eac4699352904
bylampa <157304712+bylampa@users.noreply.github.com>
2025-09-29 11:35:21 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -1,4 +1,4 @@
-if (Lampa.Storage.field('start_page') === 'history') {    
+if (Lampa.Storage.field('start_page') === 'favorite@history') {    
     var active_timer_history = setInterval(function() {    
         var my_active = Lampa.Activity.active();    
             
