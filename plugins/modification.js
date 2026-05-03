94bd870152a457f1fef58da13272d66fb9b8545f
epsill <164032791+epsill@users.noreply.github.com>
2026-05-03 17:26:09 +0500

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -38,7 +38,7 @@ localStorage.setItem('cub_domain', 'cubfix.fun');
 	      }
    })
 
-      window.lampa_settings.torrents_use = true;
+   window.lampa_settings.torrents_use = true;
    window.lampa_settings.demo = false;
    window.lampa_settings.read_only = false;
 
