aa652a2810f31d47bf560635bf1f8766726a88e1
epsill <164032791+epsill@users.noreply.github.com>
2026-05-03 17:25:34 +0500

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -38,17 +38,14 @@ localStorage.setItem('cub_domain', 'cubfix.fun');
 	      }
    })
 
-   window.lampa_settings.torrents_use = true;
+      window.lampa_settings.torrents_use = true;
    window.lampa_settings.demo = false;
    window.lampa_settings.read_only = false;
 
-   // Принудительно включаем встроенную клавиатуру после первого запуска
-   if (localStorage.getItem('force_keyboard') === 'integrate') {
-       setTimeout(function() {
-           Lampa.Storage.set('keyboard_type', 'integrate');
-           Lampa.Storage.set('system_keyboard', false);
-           localStorage.removeItem('force_keyboard');
-       }, 1000);
+   // Установка встроенной клавиатуры при первой загрузке
+   if (localStorage.getItem('lampa_keyboard') === 'integrate') {
+       Lampa.Storage.set('keyboard_type', 'integrate');
+       localStorage.removeItem('lampa_keyboard');
    }
 
    Lampa.Utils.putScriptAsync([
@@ -71,7 +68,7 @@ localStorage.setItem('cub_domain', 'cubfix.fun');
         }
     },200);
 
-   function start_set(){
+     function start_set(){
     if (Lampa.Storage.get('language') == 'ru') {
        var pluginsArray = Lampa.Storage.get('plugins','[]')
        pluginsArray.push({"author": "@bylampa","url": "https://bylampa.github.io/tmdb-proxy.js","name":"TMDB Proxy","status": 1});
@@ -79,8 +76,6 @@ localStorage.setItem('cub_domain', 'cubfix.fun');
     }
     Lampa.Storage.set('set','true');
     Lampa.Storage.set('protocol', 'http');
-    Lampa.Storage.set('keyboard_type', 'integrate');
-    Lampa.Storage.set('system_keyboard', false);
     Lampa.Storage.set('start_page', 'main');
     Lampa.Storage.set('source', 'tmdb');
     Lampa.Storage.set('background', 'false');
@@ -91,13 +86,12 @@ localStorage.setItem('cub_domain', 'cubfix.fun');
     Lampa.Storage.set('screensaver', 'false');
     Lampa.Storage.set('pages_save_total', '3');
     Lampa.Storage.set('device_name', 'Lampa Uncensored');
-	//   Lampa.Storage.set('cub_domain', 'standby.cub.red');
-	
-	// Сохраняем в localStorage чтобы после перезагрузки принудительно включить встроенную клавиатуру
-	localStorage.setItem('force_keyboard', 'integrate');
-	
-	location.reload()
-    } 
+    
+    // Клавиатура - через localStorage до перезагрузки
+    localStorage.setItem('lampa_keyboard', 'integrate');
+    
+    location.reload()
+   } 
 
      Lampa.Storage.listener.follow('change', function (event) {
       if (event.name == 'activity' && Lampa.Activity.active().component === 'bookmarks') {
